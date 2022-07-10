e9b71ad137cfd6d168066009b20fdf5803ceacde
Liam Smith <liam.s.smith@gmail.com>
2022-07-10 17:30:37 +0000

    add basic authorization to access admin product page from browser

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ProductsController < ApplicationController
-
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_AUTH_NAME"], password: ENV["HTTP_BASIC_AUTH_PSWD"]
+  
   def index
     @products = Product.order(id: :desc).all
   end
