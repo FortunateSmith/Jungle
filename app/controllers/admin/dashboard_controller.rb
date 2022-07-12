6b855f86a32b2a989d13496f12c352990518559a
Liam Smith <liam.s.smith@gmail.com>
2022-07-12 19:12:34 +0000

    add product count and category count feature to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,3 +2,14 @@ class Admin::DashboardController < ApplicationController
   def show
   end
 end
+
+# QUESTIONS * is it possible to do both product and category count with one method?
+#           * when referencing methods in erb, what decides if an instance variable or regular variable will be referenced
+
+def product_count
+  @product_count = Product.count(:id)
+end
+
+def category_count
+  @category_count = Product.count('DISTINCT category_id')
+end
