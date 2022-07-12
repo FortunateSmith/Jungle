60025b54f848ff911728ab0c0bfe5cd04d17769a
Liam Smith <liam.s.smith@gmail.com>
2022-07-12 23:41:15 +0000

    change route for test file

--- spec/requests/about_spec.rb
+++ spec/requests/about_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Abouts", type: :request do
   describe "GET /about" do
     it "returns http success" do
-      get "/about/about"
+      get "/about/index"
       expect(response).to have_http_status(:success)
     end
   end
